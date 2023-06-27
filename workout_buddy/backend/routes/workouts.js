0a5a8fd6b0cfa8b6c5c03d3df9401451943916fe
Matt Hammond <mjhammond94@gmail.com>
2023-06-27 11:54:33 +0100

    Add routes dir - Add file to routes dir

---
+++ workout_buddy/backend/routes/workouts.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', () => {
+
+})
+
+module.exports = router
+
+// The router defined on line 3 is an instance/object of express Router.
+// This router object is used to define all of our routes.
+// These routes can then be exported.
