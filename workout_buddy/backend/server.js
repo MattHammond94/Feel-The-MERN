d4bf0bceaa1794d764f2b0ff65dd3b946375cacf
Matt Hammond <mjhammond94@gmail.com>
2023-06-27 11:16:19 +0100

    Build basic backend - part 1

--- workout_buddy/backend/server.js
+++ workout_buddy/backend/server.js
@@ -1,7 +1,25 @@
 const express = require('express')
+// Line 1 requires the express package.
 
-const app = express()
+const workoutApp = express()
+// Creates an express app by calling the funct express()
+// const can be defined as anything.
 
-app.listen(4000, () => {
-  console.log('listening on port 4000')
+workoutApp.get('/', (req, res) => {
+  res.json({ message: 'Welcome' })
 })
+
+workoutApp.get('/anotherpage', (req, res) => {
+  res.json({ message: 'another message' })
+})
+// routes defined as above
+
+workoutApp.listen(4200, () => {
+  console.log('listening on port 4200')
+})
+// Port number can be defined as number. The console log here is used to show in the terminal which port is being run.
+
+// Running node Server.js from the command line starts Server.
+// Alternatively use nodemon - Nodemon will reset server each time a change is made and saved
+// in package.json line is added - "dev": "nodemon server.js"
+// This allows ```npm run dev``` to be run from command line
