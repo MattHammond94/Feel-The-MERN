7ce35bee14053a0c3e37446472977f47209f98ac
Matt Hammond <mjhammond94@gmail.com>
2023-06-27 16:24:08 +0100

    Add controllers dir - Add workoutContoller - Update routes

--- workout_buddy/backend/routes/workouts.js
+++ workout_buddy/backend/routes/workouts.js
@@ -1,25 +1,15 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+  getWorkouts,
+  getWorkout,
+  createWorkout
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Gets all workouts' })
-})
-
-router.get('/:id', (req, res) => {
-  res.json({ message: 'singular workout' })
-})
-
-router.post('/', async (req, res) => {
-  const { title, load, reps } = req.body
-  try {
-    const workout = await Workout.create({ title, load, reps })
-    res.status(200).json(workout)
-  } catch (error) {
-    res.status(400).json({ error: error.message })
-  }
-})
+router.get('/', getWorkouts)
+router.get('/:id', getWorkout)
+router.post('/', createWorkout)
 
 router.delete('/:id', (req, res) => {
   res.json({ message: 'deletes a workout' })
