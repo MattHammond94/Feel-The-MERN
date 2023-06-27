e9209780fd4760b48e9007acba546fa07f2cea60
Matt Hammond <mjhammond94@gmail.com>
2023-06-27 11:26:42 +0100

    Add .env - Add .gitignore - Install dotenv - utilise dotenv to mask env variables

--- workout_buddy/backend/server.js
+++ workout_buddy/backend/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+// require dotenv and directly invoke the config method
+
 const express = require('express')
-// Line 1 requires the express package.
+// Line 3 requires the express package.
 
 const workoutApp = express()
 // Creates an express app by calling the funct express()
@@ -14,8 +17,8 @@ workoutApp.get('/anotherpage', (req, res) => {
 })
 // routes defined as above
 
-workoutApp.listen(4200, () => {
-  console.log('listening on port 4200')
+workoutApp.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
 })
 // Port number can be defined as number. The console log here is used to show in the terminal which port is being run.
 
@@ -23,3 +26,8 @@ workoutApp.listen(4200, () => {
 // Alternatively use nodemon - Nodemon will reset server each time a change is made and saved
 // in package.json line is added - "dev": "nodemon server.js"
 // This allows ```npm run dev``` to be run from command line
+
+// ```npm install dotenv```
+// This allows us to store sensitive information within a .env file which has been added to .gitignore
+// loads env variables into a process object to be accessed accordingly.
+// process.env => An object containing all ENV variables stored within .env
