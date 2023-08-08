21b6423cca641cbf782b6b96acc9e9dc1246730a
Matt Hammond <mjhammond94@gmail.com>
2023-08-08 14:44:10 +0100

    Add events to home comp

--- React/blog/src/Home.js
+++ React/blog/src/Home.js
@@ -1,8 +1,19 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log("Hello There", e)
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target)
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
-    </div> 
+      <button onClick={ handleClick }>Click me</button>
+      <button onClick={(e) => handleClickAgain("Matt", e)}>Click me again</button>
+    </div>
   );
 }
  
