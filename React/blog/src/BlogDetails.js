e58ff8f35dd2a51c6d6528666e583775a85f8a40
Matt Hammond <mjhammond94@gmail.com>
2023-08-09 14:24:02 +0100

    update  blog details comp and add styling

--- React/blog/src/BlogDetails.js
+++ React/blog/src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch'
 
 const BlogDetails = () => {
-  const { id } = useParams()
+  const { id } = useParams();
+  const  { data: post, error, isLoading } = useFetch(`http://localhost:8000/blogs/${id}`)
 
   return ( 
     <div className="blog-details">
-      <h2>Blog details - { id }</h2>
+      { isLoading && <div>Loading...</div> }
+      { error && <div>{ error }</div>}
+      { post && (
+        <article>
+          <h2>{ post.title }</h2>
+          <p>Written by { post.author }</p>
+          <div>{ post.body }</div>
+        </article>
+      ) }
     </div>
    );
 }
