a56a3b3d5f678faf6364ed41c4dfc459c9c95968
Matt Hammond <mjhammond94@gmail.com>
2023-08-09 15:24:37 +0100

    Add useNavigate

--- React/blog/src/Create.js
+++ React/blog/src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('Matt');
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +22,8 @@ const Create = () => {
       console.log('new blog added')
       setIsPending(false);
     })
+    
+    navigate('/')
   }
 
   return ( 
