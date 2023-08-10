113a50c2940868a303691f4d681a65a6fee85e72
Matt Hammond <mjhammond94@gmail.com>
2023-08-10 11:45:50 +0100

    Add login post route

--- login/server.js
+++ login/server.js
@@ -22,4 +22,20 @@ app.post('/users', async (req, res) => {
   }
 })
 
+app.post("/users/login", async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user === null) {
+    return res.status(400).send("Cannot find user")
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success")
+    } else {
+      res.send("Password is invalid")
+    }
+  } catch {
+    res.status(500).send()
+  }
+})
+
 app.listen(420, console.log("Listening on port 420"))
