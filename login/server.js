abf644458d9c29aac3b47c94fa323e4e923dd3d8
Matt Hammond <mjhammond94@gmail.com>
2023-08-10 11:30:25 +0100

    Refactor hashing - salt generation removed

--- login/server.js
+++ login/server.js
@@ -12,9 +12,7 @@ app.get('/users', (req, res) => {
 
 app.post('/users', async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
-    console.log(salt)
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
     console.log(hashedPassword)
     const user = { name: req.body.name, password: hashedPassword }
     users.push(user)
