fdf0d547d45640c619e49804e08dadfd15967803
Matt Hammond <mjhammond94@gmail.com>
2023-08-10 11:13:18 +0100

    Add post route - update rest file to check response

--- login/server.js
+++ login/server.js
@@ -1,14 +1,18 @@
 const express = require ('express')
 const app = express()
 
-const users = [
-  { name: "Matt"},
-  { name: "Hayley" },
-  { name: "Rick Ross" }
-]
+app.use(express.json())
+
+const users = []
 
 app.get('/users', (req, res) => {
   res.json(users)
+})
+
+app.post('/users', (req, res) => {
+  const user = { name: req.body.name, password: req.body.password }
+  users.push(user)
+  res.status(201).send("User created")
 })
 
 app.listen(420, console.log("Listening on port 420"))
