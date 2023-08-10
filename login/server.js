560e8a5c9b12a331b2cf14ab93618fc22d5dcf0d
Matt Hammond <mjhammond94@gmail.com>
2023-08-10 11:05:24 +0100

    Update server - Add request rest

--- login/server.js
+++ login/server.js
@@ -1,4 +1,14 @@
 const express = require ('express')
 const app = express()
 
+const users = [
+  { name: "Matt"},
+  { name: "Hayley" },
+  { name: "Rick Ross" }
+]
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
 app.listen(420, console.log("Listening on port 420"))
