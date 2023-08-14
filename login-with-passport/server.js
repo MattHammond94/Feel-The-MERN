f65b17ab81b61fb9c3ff6dd8be2e03d4551712d9
Matt Hammond <mjhammond94@gmail.com>
2023-08-14 17:39:31 +0100

    add vieww folder - update routes

--- login-with-passport/server.js
+++ login-with-passport/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.use('/login', (req, res) => {
-  res.send("Login page")
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send("This is a redundant homepage")
+})
+
+app.get('/login', (req, res) => {
+  res.render('index.ejs')
 })
 
 app.listen(2400, console.log("Listening on port 2400"))
