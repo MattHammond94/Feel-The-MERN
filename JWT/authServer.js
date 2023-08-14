dc5dd5cc8243a5f09ef9319e12150495e56340b9
Matt Hammond <mjhammond94@gmail.com>
2023-08-14 15:59:55 +0100

    Refactor AuthServer - add refreshToken

--- JWT/authServer.js
+++ JWT/authServer.js
@@ -4,14 +4,8 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(logger)
 
 const JWT = require('jsonwebtoken')
-
-app.get('/posts', authenticateToken, (req, res) => {
-  req.user
-  res.json(posts.filter(post => post.username === req.user.name))
-})
 
 app.post('/login', (req, res) => { 
   // First of all authenticate user as per previous project in login folder
@@ -19,22 +13,13 @@ app.post('/login', (req, res) => {
   const username = req.body.username
   const user = { name: username }
 
-  const accessToken = JWT.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json({accessToken: accessToken })
+  const accessToken = generateAccessToken(user)
+  const refreshToken = JWT.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  res.json({accessToken: accessToken, refreshToken: refreshToken })
 })
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  console.log(authHeader)
-  const token = authHeader && authHeader.split(" ")[1]
-  console.log(token)
-  if (token === null) return res.sendStatus(401)
-
-  JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
+function generateAccessToken(user) { 
+  return JWT.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
 }
 
 app.listen(1000, console.log("Listening on port 1000(Auth Server)"))
