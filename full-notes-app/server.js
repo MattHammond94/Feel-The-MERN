a2a0c530eac36e123682e77e7a969d39fe9db61c
Matt Hammond <mjhammond94@gmail.com>
2023-08-15 11:53:51 +0100

    Update server

--- full-notes-app/server.js
+++ full-notes-app/server.js
@@ -3,9 +3,14 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 4000
 
+app.use(express.json())
+
 // This line tells the server where to locate static files such as CSS/images
 // path imported on line 3
 app.use('/', express.static(path.join(__dirname, '/public')))
+
+// app.use(express.static('public')) => Alternate/refactored
+// The above alternate will only work if the file path is relative.
 
 app.use('/', require('./routes/root'))
 
