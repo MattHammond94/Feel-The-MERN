23887e7f3236a13261497b34d8c4096f33f0d3a6
Matt Hammond <mjhammond94@gmail.com>
2023-08-15 11:01:41 +0100

    Create basic file structure for splash page

--- full-notes-app/server.js
+++ full-notes-app/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 4000
 
-app.use()
+// This line tells the server where to locate static files such as CSS/images
+// path imported on line 3
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'))
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`))
