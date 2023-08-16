e7e9ad9b61044a7c0b0dd77383a1e21b7a7bbd77
Matt Hammond <mjhammond94@gmail.com>
2023-08-16 17:57:37 +0100

    Add readnwrite files

---
+++ NODE/ReadandWriteFiles.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+// Both the below read file methods work however the first is better
+fs.readFile(path.join(__dirname, 'files', 'text1.txt'), 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data)
+})
+
+fs.readFile('./files/text.txt', (err, data) => {
+  if (err) throw err;
+  console.log(data.toString())
+})
+
+// Having the appendFile and rename within the same call ensures the file exists first before appending to it.(Stops issues caused by async)
+// --------------------------------------
+
+// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'A new text file', (err) => {
+//   if (err) throw err;
+//   console.log("Write completed")
+
+//   fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), ', Tester', (err) => {
+//     if (err) throw err;
+//     console.log("Append completed")
+
+//     fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newName.txt'), (err) => {
+//       if (err) throw err;
+//       console.log("Rename complete")
+//     })
+//   })
+// })
+
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'text.txt'), 'utf8')
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fileOps()
+
+process.on('uncaughtException', err => {
+  console.error(`There was an uncaught error: ${err}`);
+  process.exit(1);
+})
