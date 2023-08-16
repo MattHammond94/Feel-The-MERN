45e7619ad396910d32e3ff26700c330e5869555c
Matt Hammond <mjhammond94@gmail.com>
2023-08-16 16:02:44 +0100

    Minor changes

--- full-notes-app/frontend/src/components/Public.js
+++ full-notes-app/frontend/src/components/Public.js
@@ -7,7 +7,7 @@ const Public = () => {
                 <h1>Welcome to <span className="nowrap">Matts Repairs!</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Land :Matts Reapirs: provides a trained staff ready to meet your tech repair needs.</p>
+                <p>Located in Beautiful BiddlyBopLand - Matts Reapirs provides a trained staff ready to meet your tech repair needs.</p>
                 <address className="public__addr">
                     Matts Repairs<br />
                     444 four Drive<br />
