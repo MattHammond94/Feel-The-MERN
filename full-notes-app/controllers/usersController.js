f1e22bdbb489d4fb0e87d351bdee3fe9b2dee147
Matt Hammond <mjhammond94@gmail.com>
2023-08-16 10:37:26 +0100

    Add createNewUser logic in controller

--- full-notes-app/controllers/usersController.js
+++ full-notes-app/controllers/usersController.js
@@ -3,8 +3,6 @@ const User = require('../models/Note')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
 
-
-
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select('-password').lean()
   if (!users) {
@@ -15,7 +13,29 @@ const getAllUsers = asyncHandler(async (req, res) => {
 
 
 const createNewUser = asyncHandler(async (req, res) => {
+  const { username, password, roles } = req.body
+
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
   
+  const duplicate = await User.findOne({ username }).lean().exec()
+
+  if(duplicate) {
+    return res.status(409).json({ message: 'Duplicate username' })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const userObject = { username, "password": hashedPassword, roles}
+
+  const user = await User.create(userObject)
+
+  if (user) {
+    res.status(201).json({ message: `New user ${username} created` })
+  } else {
+    res.status(400).json({ message: 'Invalid user data received' })
+  }
 })
 
 
