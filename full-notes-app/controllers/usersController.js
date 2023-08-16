29001ddcff20cd60471524ae25612d3a28fcd246
Matt Hammond <mjhammond94@gmail.com>
2023-08-16 10:26:53 +0100

    Establish user routes - update user controller

--- full-notes-app/controllers/usersController.js
+++ full-notes-app/controllers/usersController.js
@@ -4,3 +4,33 @@ const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
 
 
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().select('-password').lean()
+  if (!users) {
+    return res.status(400).json({ message: 'No users found' })
+  }
+  res.json(users)
+})
+
+
+const createNewUser = asyncHandler(async (req, res) => {
+  
+})
+
+
+const updateUser = asyncHandler(async (req, res) => {
+  
+})
+
+
+const deleteUser = asyncHandler(async (req, res) => {
+  
+})
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser
+}
