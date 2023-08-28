e0acfa3b12cc78cbb6b372703ec4ff683433b4a9
Matt Hammond <mjhammond94@gmail.com>
2023-08-28 16:27:37 +0100

    Refactor app.js to use modern router approach (6.4+) - Add layout for root

--- Router-Depth/src/App.js
+++ Router-Depth/src/App.js
@@ -1,27 +1,54 @@
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import { 
+  createBrowserRouter,
+  Route, 
+  NavLink, 
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+
+// pages:
 import Home from './pages/Home';
 import About from './pages/About';
 
+// Layouts:
+import RootLayout from './layouts/RootLayout';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
 
 function App() {
-  return (
-
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>JobBoard</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return(
+    <RouterProvider router={router} />
+  )
 }
+
+// ----- The Below is now considered outdated as of V6.4+ ------
+// function App() {
+//   return (
+
+//     <BrowserRouter>
+//       <header>
+//         <nav>
+//           <h1>JobBoard</h1>
+//           <NavLink to="/">Home</NavLink>
+//           <NavLink to="about">About</NavLink>
+//         </nav>
+//       </header>
+//       <main>
+//         <Routes>
+//           <Route index element={<Home />} />
+//           <Route path="about" element={<About />} />
+//         </Routes>
+//       </main>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App
