0f646c8a95df08b4d409bf50803337cd099bd530
Matt Hammond <mjhammond94@gmail.com>
2023-08-28 11:50:35 +0100

    Add SignUp comp - Install router - update index to add new routes - amend app.js

--- firebase_v2/firebasev2/src/App.js
+++ firebase_v2/firebasev2/src/App.js
@@ -1,9 +1,16 @@
 import SignIn from './components/auth/SignIn'
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <h1>This is our new homepage!</h1>
+      <div>
+        <Link to={'/signup'}>Sign Up</Link>
+      </div>
+      <div>
+        <Link to={'/login'}>Log In</Link>
+      </div>
     </div>
   );
 }
--- firebase_v2/firebasev2/src/index.js
+++ firebase_v2/firebasev2/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SignUp from './components/SignUp'
+import SignIn from './components/auth/SignIn'
+import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element ={<App />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
