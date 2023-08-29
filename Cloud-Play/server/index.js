e954803d10f8002fa4d4660b0950ac7d3b10fbb7
Matt Hammond <mjhammond94@gmail.com>
2023-08-29 16:03:52 +0100

    Update cloudinary file to config cloudinary

--- Cloud-Play/server/index.js
+++ Cloud-Play/server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT
 const cors = require("cors");
+const cloudinary = require("./cloudinary/cloudinary")
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
