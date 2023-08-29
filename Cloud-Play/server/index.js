a2f6220072e59cd291a877863cdd104d2ac71ee7
Matt Hammond <mjhammond94@gmail.com>
2023-08-29 15:25:57 +0100

    Set up server

--- Cloud-Play/server/index.js
+++ Cloud-Play/server/index.js
@@ -2,3 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const port = process.env.PORT
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage")
+})
+
+app.post("/", async (req, res) => {
+
+})
+
+app.listen(port, console.log(`listening on port ${port}`))
