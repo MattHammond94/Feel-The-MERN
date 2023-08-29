33896c221e138afaf0eb8efe18bc0129fb628abb
Matt Hammond <mjhammond94@gmail.com>
2023-08-29 14:30:22 +0100

    Add navigate comp to about

--- Router-Depth/src/pages/About.js
+++ Router-Depth/src/pages/About.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import { Navigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const About = () => {
+  const [user, setUser] = useState('Matty')
+
+  if (!user) {
+    return <Navigate to="/" replace={true} />
+  }
+
   return (
     <div className="about">
-      <h2>About Page!</h2>
+      <h2>About Us!</h2>
       <p>lorem</p>
+
+      <button onClick={() => setUser(null)}>Log out</button>
     </div>
   )
 }
