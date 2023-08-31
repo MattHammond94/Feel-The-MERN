88f3897086c23f2b11664e020e84805daaab4aee
Matt Hammond <mjhammond94@gmail.com>
2023-08-31 15:11:05 +0100

    Add post route to app - Add frontend react app

--- imgZ/cloud2/backend/app.js
+++ imgZ/cloud2/backend/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require("cors");
+const uploadImage = require("./uploadImage");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({  limit: "25mb"}));
 app.use(express.urlencoded({  limit: "25mb" }));
+
+app.post('/uploadImage', (req, res) => {
+
+  uploadImage(req.body.image)
+    .then((url) => res.send(url))
+    .catch((err) => res.status(500).send(err));
+});
 
 app.listen(44444, console.log('Listening on port 44444'));
