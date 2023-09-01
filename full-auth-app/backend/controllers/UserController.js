71d2f7d4edb09f7fe1ba7089ebbec0ae9aa9e0c3
Matt Hammond <mjhammond94@gmail.com>
2023-09-01 11:03:16 +0100

    Add all paths to controller - update routes for user

--- full-auth-app/backend/controllers/UserController.js
+++ full-auth-app/backend/controllers/UserController.js
@@ -1,14 +1,44 @@
 import asyncHandler from 'express-async-handler'
 
-// Authenticates user and sets token
-// POST /api/users/auth
-// public
+//  Authenticates user and sets token
+//  POST /api/users/auth
+//  public
 const authUser = asyncHandler((req, res) => {
   res.status(200).json({ message: 'Auth User' })
 })
 
+//  Register a new user
+//  POST /api/users
+//  public
+const registerUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'Register User' })
+})
 
+//  Logout user
+//  POST /api/users/logout
+//  public
+const logoutUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'Logout User' })
+})
+
+//  User Profile
+//  GET /api/users/profile
+//  private
+const getUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'User profile' })
+})
+
+//  Update user profile
+//  PUT/PATCH - /api/users/profile
+//  private
+const updateUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: 'Update user profile' })
+})
 
 export {
-  authUser
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile
 };
--- full-auth-app/backend/routes/userRoutes.js
+++ full-auth-app/backend/routes/userRoutes.js
@@ -1,7 +1,19 @@
 import express from 'express'
 const router = express.Router();
-import { authUser } from '../controllers/UserController.js';
+import { authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile 
+} from '../controllers/UserController.js';
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/auth/logout', logoutUser);
+
+// router.get('/profile', getUserProfile);
+// router.patch('/profile', updateUserProfile);
+// The below does the same as the two lines above.
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
