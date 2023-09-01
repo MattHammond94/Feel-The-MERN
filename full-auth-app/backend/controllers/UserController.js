b3cdaf69008ac1b04c5680bacb4440d1e6d5b0b8
Matt Hammond <mjhammond94@gmail.com>
2023-09-01 14:52:01 +0100

    Add log out funct

--- full-auth-app/backend/controllers/UserController.js
+++ full-auth-app/backend/controllers/UserController.js
@@ -60,8 +60,14 @@ const registerUser = asyncHandler(async (req, res) => {
 //  Logout user
 //  POST /api/users/logout
 //  public
-const logoutUser = asyncHandler((req, res) => {
-  res.status(200).json({ message: 'Logout User' })
+const logoutUser = asyncHandler(async (req, res) => {
+
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+
+  res.status(200).json({ message: 'User logged out' })
 })
 
 //  User Profile
