6e6e332953629dc0593d940be2dc0bc3f873a10a
Matt Hammond <mjhammond94@gmail.com>
2023-09-01 10:45:47 +0100

    Add errorMiddleware - install async handler - update server - update userCont

--- full-auth-app/backend/controllers/UserController.js
+++ full-auth-app/backend/controllers/UserController.js
@@ -1,9 +1,13 @@
+import asyncHandler from 'express-async-handler'
+
 // Authenticates user and sets token
 // POST /api/users/auth
 // public
-const authUser = (req, res) => {
+const authUser = asyncHandler((req, res) => {
   res.status(200).json({ message: 'Auth User' })
-}
+})
+
+
 
 export {
   authUser
--- full-auth-app/backend/server.js
+++ full-auth-app/backend/server.js
@@ -3,10 +3,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 4444
 
-const app = express()
+import userRoutes from './routes/userRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+
+const app = express();
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Homepage')
-})
+});
+
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
