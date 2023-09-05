6953416cf0bd0dad878e9609571173184bf34813
Matt Hammond <mjhammond94@gmail.com>
2023-09-05 17:20:01 +0100

    add register mutation to apislice - update register screen

--- full-auth-app/frontend/src/slices/usersApiSlice.js
+++ full-auth-app/frontend/src/slices/usersApiSlice.js
@@ -4,6 +4,13 @@ const USERS_URL = '/api/users';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: 'POST',
+        body: data
+      }),
+    }),
     login: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}/auth`,
@@ -20,4 +27,4 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
