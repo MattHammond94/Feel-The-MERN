33ef4f06224fb39833664f1ceb8025bb902df539
Matt Hammond <mjhammond94@gmail.com>
2023-10-09 12:31:35 +0100

    Add arrow elements to slider

--- img-slider/src/ImageSlider.js
+++ img-slider/src/ImageSlider.js
@@ -16,10 +16,45 @@ const ImageSlider = ({slides}) => {
     backgroundSize: 'cover',
     backgroundImage: `url(${slides[currentIndex].url})`
   }
+  
+  const leftArrowStyles = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translate(0, -50%)',
+    left: '20px',
+    fontSize: '50px',
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer"
+  }
 
+  const rightArrowStyles = {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translate(0, -50%)',
+    right: '18px',
+    fontSize: '50px',
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer"
+  }
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex -1
+    setCurrentIndex(newIndex);
+  }
+
+  const goToNext = () => {
+    const isFinalSlide = currentIndex === 4
+    const newIndex = isFinalSlide ? 0 : currentIndex + 1
+    setCurrentIndex(newIndex);
+  }
   
   return (
     <div style={sliderStyles}>
+      <div style={leftArrowStyles} onClick={goToPrevious}>←</div>
+      <div style={rightArrowStyles} onClick={goToNext}>→</div>
       <div style={slideStyles}></div>
     </div>
   )
