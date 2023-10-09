4b62a7431a33f5d3a8e6550e605e061fddcc53a5
Matt Hammond <mjhammond94@gmail.com>
2023-10-09 12:06:49 +0100

    Create ImageSlider component - update app.js

--- img-slider/src/App.js
+++ img-slider/src/App.js
@@ -1,8 +1,27 @@
+import ImageSlider from './ImageSlider';
 
 function App() {
+
+  const slides = [
+    { url: 'http://localhost:3000/1.jpg', title: 'Artwork-1' },
+    { url: 'http://localhost:3000/2.jpg', title: 'Artwork-2' },
+    { url: 'http://localhost:3000/3.jpg', title: 'Artwork-3' },
+    { url: 'http://localhost:3000/4.jpg', title: 'Artwork-4' },
+    { url: 'http://localhost:3000/5.jpg', title: 'Artwork-5' }
+  ]
+
+  const containerStyles = {
+    width: '500px',
+    height: '600px',
+    margin: '0 auto'
+  };
+
   return (
     <div>
-      <h1>Appppp</h1>
+      <h1>Image Slider</h1>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
     </div>
   );
 }
